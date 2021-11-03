1b439a7906e16fb8e431a82be47be1a7ba1b7cc0
dennislederich <dennis.lederich@lgln.niedersachsen.de>
2021-11-03 16:34:28 +0100

    change fzf behavior and fix initial nvim setup - 2nd try

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -1,8 +1,7 @@
 -- init.lua
-
 require('settings')    -- lua/settings.lua
 require('bindings')    -- lua/bindings.lua (maps)
+require('plugins')     -- lua/plugins.lua
 require('statusline')  -- lua/statusline.lua
 require('theme')       -- lua/theme.lua
-require('plugins')     -- lua/plugins.lua
 require('configs')     -- lua/configs.lua
